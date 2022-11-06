3e66eeb3e66c4891fe32d5ce2d7703fa3ba15e51
PiotrWendzierski <piotrwendzierski@gmail.com>
2022-11-06 21:48:15 +0100

    dodanie arkusza styli i karty do dodawania transakcji

--- dodawanie.php
+++ dodawanie.php
@@ -2,7 +2,7 @@
 <html lang="pl">
 <head>
 	<meta charset="utf-8">
-	<title>eWallet - twój elektroniczny portfel</title>
+	<title>eWallet - dodaj transakcję</title>
 	<link rel="stylesheet"  href="style.css" type="text/css" / >
 </head>
 <body>
@@ -10,13 +10,12 @@
 	<div id="title">
 		eWallett
 	</div>
-	<div id="meni">		
-		<a href="dodawanie.php">+</a>
-	</div>
-	<div id="dashboard">
-		<div class="kafelek">1</div>
-		<div class="kafelek">2</div>
-		<div style="clear:both"></div>
+	<div id="formularz">
+		<form method="post">
+			Wpisz kategorię<input type="text" name="kategoria"> </br></br>
+			Wpisz cenę<input type="number" name="kategoria">  </br></br>
+			Zatwierdź <input type="submit">
+		</form>
 	</div>
 	<div id="footer">Wszelkie prawa zastrzeżone</div>
 </div>

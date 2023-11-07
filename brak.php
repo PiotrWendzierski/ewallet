f7d1344e5a9fe13be95926572e70e14c66467b9c
PiotrWendzierski <piotrwendzierski@gmail.com>
2023-11-07 23:21:01 +0100

    ukodowanie pełnej strony z wykresem dot. wszystkich wydatków i filtrowanie na niej dat

---
+++ brak.php
@@ -0,0 +1,55 @@
+<?php
+session_start();
+if(!isset($_SESSION['zalogowany']))
+{
+	header('Location: login.php');
+	exit();
+}
+if($_COOKIE['i'] != 0)
+{
+	header('Location: login.php');
+	exit();
+}
+?><!DOCTYPE HTML>
+<html lang="pl">
+<head>
+	<meta charset="utf-8">
+	<title>eWallet - twój elektroniczny portfel</title>
+	<link rel="stylesheet"  href="style.css" type="text/css" / >
+	<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+</head>
+<body>
+<div id="container">
+	<div id="title">
+		eWallett
+	</div>
+	<div id="meni">		
+	<ol>
+		<li><a href="index2.php">Ekran główny</a></li>
+		<li><a href="dodawanie.php">Nowa transakcja</a></li>
+		<li><a href="historia.php">Historia portfela</a></li>
+		<li><a href="#">Skarbonka</a>
+					<ul>
+						<li><a href="skarbonka.php">Dodaj transakcję</a></li>
+						<li><a href="historia_skarbonki.php">Historia skarbonki</a></li>
+					</ul>
+				</li>
+		<li><a href="wyloguj.php">Wyloguj</a></li>
+	</ol>
+	</div>
+	<div id = "brak">
+	<form action="kategorie_wydatkow.php" method = "POST">
+			<?php
+			if((isset($_COOKIE['i'])) && ($_COOKIE['i'] == 0))
+			{
+				setcookie('i', 1);
+				echo '<span style="color:red">Brak wydatków w tym okresie!</span>'.'</br>';
+			}
+			
+			?>
+			<input type="submit" value="Wróć"></input>
+	</form>
+	</div>
+</body>
+
+</html>

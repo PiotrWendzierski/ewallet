967b63f781126c5eceb654b7d41b867c64a18830
PiotrWendzierski <piotrwendzierski@gmail.com>
2023-11-14 22:47:54 +0100

    zrobienie tego samego z wydatkami

--- kategorie_wplywow.php
+++ kategorie_wplywow.php
@@ -10,6 +10,21 @@ if(!isset($_SESSION['stan_konta'])|| ($_SESSION['stan_konta'] ==false))
 	header('Location: index.php');
 	exit();
 }
+
+require_once("connect.php");
+$od = "0000-01-01"; if(isset($_GET['od']))$od = $_GET['od'];
+$do =  "6000-01-01"; if(isset($_GET['do']))$do = $_GET['do']; 
+if(isset($_GET['od'])&& ($_GET['od'] == ""))$od = "0000-01-01";
+if(isset($_GET['do'])&& ($_GET['do'] == ""))$do = "6000-01-01";
+
+$id = $_SESSION['id'];
+$polaczenie = new mysqli($host, $db_user, $db_password, $db_name);
+
+ $sql = "SELECT * FROM kategorie WHERE id = '$id' AND wplywwyplyw = 'wplyw' ";
+
+$rezultat = mysqli_query($polaczenie,$sql);
+
+
 ?>
 <!DOCTYPE HTML>
 <html lang="pl">
@@ -17,6 +32,7 @@ if(!isset($_SESSION['stan_konta'])|| ($_SESSION['stan_konta'] ==false))
 	<meta charset="utf-8">
 	<title>eWallet - twój elektroniczny portfel</title>
 	<link rel="stylesheet"  href="style.css" type="text/css" / >
+	<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 </head>
 <body>
 <div id="container">
@@ -37,43 +53,75 @@ if(!isset($_SESSION['stan_konta'])|| ($_SESSION['stan_konta'] ==false))
 		<li><a href="wyloguj.php">Wyloguj</a></li>
 	</ol>
 	</div>
+	<div>
+	<a href="kategorie_wplywowprocent.php">POKAŻ WYKRES KWOTOWY</a>
+	<form id = "okres" action="" method = "GET">
+			Wybierz okres</br>
+			<input type="date" name = "od" value="<?php if(isset($_GET['od'])){echo $_GET['od'];}?>"/> 
+			<input type="date" name = "do" value="<?php if(isset($_GET['do'])){echo $_GET['do'];}?>"/> 
+			<?php
+			if((isset($_COOKIE['i'])) && ($_COOKIE['i'] == 0))
+			{
+				header('Location: brak.php');
+				exit();
+			}
+			
+			?>
+			<input type="submit" value="Filtruj"></input>
+	</form>
+	</div>
 	<div id="kategorie_wydatkow">
-	<?php
-	require_once "connect.php";
-	try
-	{
-		$polaczenie = new mysqli($host, $db_user, $db_password, $db_name);
-		if($polaczenie->connect_errno!=0)
-		{
-			throw new Exception(mysqli_connect_errno());
-		 }
-		 else
-		 {
-			 $id = $_SESSION['id'];
-			 $sql = "SELECT * FROM kategorie WHERE id = '$id' AND wplywwyplyw = 'wplyw'";
-			 $rezultat = $polaczenie -> query ($sql);
-			 $ile_kategorii = $rezultat -> num_rows;
-			 if($ile_kategorii !=0)
-			 {
+	<div id="piechart" style="width: 900px; height: 500px; margin-left: auto; margin-right: auto; ">
+	<script type='text/javascript'>
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['Task', 'Hours per Day'],
+		 <?php
+			$ile_kategorii = $rezultat -> num_rows; 
+			if($ile_kategorii >0)
+			{
+				$i = 0;
 				 while($row = $rezultat -> fetch_assoc())
 				 {
 					 $kategoria = $row['kategoria'];
-					 $ilosc_transakcji = $row['ilosc_transakcji'];
-					 echo $kategoria."   ".$ilosc_transakcji."</br>";
+					 $sql5 = "SELECT * FROM transakcje WHERE id ='$id' AND wplywwyplyw = 'wplyw' AND kategoria = '$kategoria' AND data BETWEEN '$od'
+					 AND '$do'";
+					 $rezultat2 = $polaczenie ->query($sql5);
+					 $laczna_ilosc_transakcji = 0;
+					 while($row2 = $rezultat2->fetch_assoc())
+					{
+						$i++;
+						$laczna_ilosc_transakcji=$laczna_ilosc_transakcji+1;
+					 }
+					 echo "['".$row['kategoria']."', ".$laczna_ilosc_transakcji."],";
+					 
 				 }
-			 }
-			 else echo "Brak wpływów !";
-			 
-		 }
-		 $polaczenie -> close();
-	}
-	catch (Exception $e)
-	{
-		echo $e;
-	 }
-	
-	?>
-	</div>
+				 if($i == 0)
+				 { 
+					 setcookie('i', 0);
+					 header('Location: kategorie_wplywow.php');
+				 }
+			}
+		 ?>
+        ]);
+
+        var options = {
+          legend: 'none',
+		  chartArea:{top:50,width:'90%',height:'80%'},
+		  backgroundColor: '#F6F3F3',
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+        chart.draw(data, options);
+      }
+    </script>
+
+</div>
 	<div id="footer">Wszelkie prawa zastrzeżone</div>
 </div>
 </body>
